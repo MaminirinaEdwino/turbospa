76486846311c7ee1a736fee5f025087895857afd
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-22 16:41:38 +0300

    add: refresh system

--- engine.go
+++ engine.go
@@ -8,18 +8,18 @@ import (
 	"syscall/js"
 )
 
-func Mount(targetID string, comp Component) {
-	document := js.Global().Get("document")
-	root := document.Call("getElementById", targetID)
+// func Mount(targetID string, comp Component) {
+// 	document := js.Global().Get("document")
+// 	root := document.Call("getElementById", targetID)
 
-	// Premier rendu
-	vnode := comp.Render()
-	domEl := createDOMElement(vnode)
-	root.Call("appendChild", domEl)
+// 	// Premier rendu
+// 	vnode := comp.Render()
+// 	domEl := createDOMElement(vnode)
+// 	root.Call("appendChild", domEl)
 
-	// Bloquer le programme pour maintenir la SPA active
-	select {}
-}
+// 	// Bloquer le programme pour maintenir la SPA active
+// 	select {}
+// }
 func createDOMElement(vnode VNode) js.Value {
 	fmt.Printf("Création de l'élément : [%s]\n", vnode.TagName)
     doc := js.Global().Get("document")
@@ -56,30 +56,27 @@ func createDOMElement(vnode VNode) js.Value {
     return el
 }
 
-// func createDOMElement(vnode VNode) js.Value {
-// 	doc := js.Global().Get("document")
-// 	tag := strings.TrimSpace(vnode.TagName)
-// 	el := doc.Call("createElement", tag)
-// 	// el := doc.Call("createElement", vnode.TagName)
+// turbospa/engine.go
 
-// 	// Appliquer les attributs (id, class, etc.)
-// 	for k, v := range vnode.Attrs {
-// 		el.Set(k, v)
-// 	}
 
-// 	// Gérer le texte ou les enfants
-// 	if vnode.Text != "" {
-// 		el.Set("innerText", vnode.Text)
-// 	}
 
-// 	for _, child := range vnode.Children {
-// 		el.Call("appendChild", createDOMElement(child))
-// 	}
-// 	for eventName, handler := range vnode.Events {
-// 		// On transforme la fonction Go en fonction compatible JS
-// 		jsCallback := js.FuncOf(handler)
-// 		// On l'attache (ex: "click", "input")
-// 		el.Call("addEventListener", eventName, jsCallback)
-// 	}
-// 	return el
-// }
+var Instance *Core
+
+func Mount(targetID string, comp Component) {
+    document := js.Global().Get("document")
+    root := document.Call("getElementById", targetID)
+
+    // Premier rendu
+    vnode := comp.Render()
+    domEl := createDOMElement(vnode)
+    root.Call("appendChild", domEl)
+
+    // On initialise l'instance globale pour pouvoir appeler Update() partout
+    Instance = &Core{
+        rootElement:  root,
+        currentVNode: &vnode,
+        app:          comp,
+    }
+
+    select {}
+}
